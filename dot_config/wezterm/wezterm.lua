d920f1ffa3bb3d30e5731af6554f6ad09f93ddc1
Binh Duc Tran <binhtran432k@gmail.com>
2023-12-27 18:53:32 +0700

    feat(wezterm): add font/theme/layout config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,6 +2,33 @@ local wezterm = require "wezterm"
 
 local config = {}
 
-config.enable_wayland = false
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.enable_wayland = false -- FIXME: remove when wezterm wayland working again
+
+-- This is where you actually apply your config choices
+-- config.font = wezterm.font({ family = "Cascadia Code", harfbuzz_features = { "calt=1", "ss01=1" } })
+config.font = wezterm.font({ family = "Maple Mono" })
+
+config.bold_brightens_ansi_colors = true
+
+config.color_scheme = "dracula"
+
+config.window_background_opacity = 0.7
+config.text_background_opacity = 0.8
+config.window_padding = {
+  top = 0,
+  left = 0,
+  bottom = 0,
+  right = 0,
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.warn_about_missing_glyphs = false
 
 return config
