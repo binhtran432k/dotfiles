6dfd5bc4a5564b193ff956178a1b7297c5b6f0ed
Binh Tran <binhtran432k@gmail.com>
2024-11-05 06:41:04 +0000

    feat(nvim/cmp): add lspkind

--- nvim/lua/binhtran432k/plugins/cmp.lua
+++ nvim/lua/binhtran432k/plugins/cmp.lua
@@ -8,15 +8,41 @@ return {
       --  into multiple repos for maintenance purposes.
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-buffer",
+      "onsails/lspkind.nvim",
     },
     config = function()
       -- See `:help cmp`
       local cmp = require("cmp")
+      local lspkind = require("lspkind")
       local cmp_float_window = cmp.config.window.bordered({
         winhighlight = "CursorLine:Visual,Search:None",
       })
       cmp.setup({
         completion = { completeopt = "menu,menuone,noinsert" },
+
+        ---@diagnostic disable-next-line: missing-fields
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text", -- show only symbol annotations
+            maxwidth = {
+              -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+              -- can also be a function to dynamically calculate max width such as
+              -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+              menu = function()
+                return math.min(math.floor(0.6 * vim.o.columns), 50)
+              end, -- leading text (labelDetails)
+              abbr = 50, -- actual suggestion item
+            },
+            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(entry, vim_item)
+              return vim_item
+            end,
+          }),
+        },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
