be8255de780765435e18a52b85b29918d69402d7
Binh Tran <binhtran432k@gmail.com>
2024-11-21 07:32:01 +0000

    feat(nvim/lang): make tailwind lsp need config to run

--- nvim/lua/plugins/lang.lua
+++ nvim/lua/plugins/lang.lua
@@ -49,6 +49,20 @@ return {
         biome = {},
         cssls = {},
         html = {},
+        tailwindcss = {
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "tailwind.config.js",
+              "tailwind.config.cjs",
+              "tailwind.config.mjs",
+              "tailwind.config.ts",
+              "postcss.config.js",
+              "postcss.config.cjs",
+              "postcss.config.mjs",
+              "postcss.config.ts"
+            )(fname)
+          end,
+        },
       },
     },
   },
