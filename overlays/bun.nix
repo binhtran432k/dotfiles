e7cd4c46e1aa0bba34f3fbc94ba82d2f878380cb
Binh Tran <binhtran432k@gmail.com>
2024-11-26 07:28:14 +0000

    feat(overlays): upgrade bun to v1.1.36

--- overlays/bun.nix
+++ overlays/bun.nix
@@ -1,8 +1,8 @@
 {prev, ...}:
 prev.bun.overrideAttrs (oldAttrs: rec {
-  version = "1.1.34";
+  version = "1.1.36";
   src = prev.fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-S8AA/1CWxTSHZ60E2ZNQXyEAOalYgCc6dte9CvD8Lx8=";
+    hash = "sha256-xSYfTX40L+cgvGpdc2sclPTfKULCJah2UsaYUYGx7Hc=";
   };
 })
