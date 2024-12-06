653bf669b134b8c8cf01f168f193f9eda74a01c0
Binh Tran <binhtran432k@gmail.com>
2024-12-06 11:36:44 +0700

    fix(nvim/root_spec): add node_modules

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 
 vim.g.autoformat = false
 
-vim.g.root_spec = { { ".git", "lua", "Makefile" }, "lsp", "cwd" }
+vim.g.root_spec = { { ".git", "lua", "Makefile", "node_modules" }, "lsp", "cwd" }
 
 vim.g.markdown_fenced_languages = {
   "ts=typescript",
