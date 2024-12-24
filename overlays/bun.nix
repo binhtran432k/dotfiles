782041d0015199e2e1a957b3e85b7e2e12f667b2
Binh Tran <binhtran432k@gmail.com>
2024-12-24 13:55:39 +0700

    fix(overlays): remove bun

--- overlays/bun.nix
+++
@@ -1,8 +0,0 @@
-{prev, ...}:
-prev.bun.overrideAttrs (oldAttrs: rec {
-  version = "1.1.36";
-  src = prev.fetchurl {
-    url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-xSYfTX40L+cgvGpdc2sclPTfKULCJah2UsaYUYGx7Hc=";
-  };
-})
--- overlays/default.nix
+++ overlays/default.nix
@@ -10,7 +10,6 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-    bun = import ./bun.nix {inherit final prev;};
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
