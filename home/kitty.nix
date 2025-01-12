202ec8de90f72ddc54fc51ed440df7ff143f1c23
Binh Tran <binhtran432k@gmail.com>
2025-01-12 19:14:29 +0700

    fix(kitty): add ligature cursor and fix symbol_map

--- home/kitty.nix
+++ home/kitty.nix
@@ -8,7 +8,8 @@
     themeFile = "Dracula";
     settings = {
       cursor_trail = 1;
-      symbol_map = "U+23FB-U+23FE,U+2665,U+26a1,U+2b58,U+e000-U+e00a,U+e0a0-U+e0a2,U+e0a3,U+e0b0-U+e0b3,U+e0b4-U+e0c8,U+e0ca,U+e0cc-U+e0d4,U+e200-U+e2a9,U+e300-U+e3eb,U+e5fa-U+e631,U+e700-U+e7c5,U+ea60-U+ebeb,U+f000-U+f2e0,U+f300-U+f32f,U+f400-U+f4a8,U+f4a9,U+f500-U+fd46 Symbols Nerd Font Mono";
+      disable_ligatures = "cursor";
+      symbol_map = "U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono";
     };
   };
 }
