7ea863ef57ad5dc0a79e482baeae3361ccd1193b
Binh Tran <binhtran432k@gmail.com>
2025-02-06 11:45:14 +0700

    refactor(nvim/lang): split in to file scope

--- nvim/lua/plugins/lang.lua
+++ nvim/lua/plugins/lang.lua
@@ -1,15 +1,6 @@
 return {
-  {
-    "tree-sitter-grammars/tree-sitter-test",
-    build = "mkdir parser && tree-sitter build -o parser/test.so",
-    ft = "test",
-    init = function()
-      -- toggle full-width rules for test separators
-      vim.g.tstest_fullwidth_rules = false
-      -- set the highlight group of the rules
-      vim.g.tstest_rule_hlgroup = "FoldColumn"
-    end,
-  },
+  { import = "plugins.lang.test" },
+  { import = "plugins.lang.gherkin" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "css", "html" } },
