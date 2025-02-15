41f7ca63354fe40496bc9111fd11366f5b898869
Binh Tran <binhtran432k@gmail.com>
2025-02-15 09:31:50 +0700

    feat(nvim): add oil.nvim

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,2 +1,11 @@
 return {
+  {
+    "stevearc/oil.nvim",
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+    },
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+  },
 }
