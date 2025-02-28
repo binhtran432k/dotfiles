18df8c924995ab3debeae6686ce91b3fa09f02ca
Binh Tran <binhtran432k@gmail.com>
2025-02-28 04:20:36 +0000

    feat(wezterm/fonts): improve ux

--- home/wezterm/wezterm.lua
+++ home/wezterm/wezterm.lua
@@ -3,8 +3,9 @@ local wezterm = require("wezterm")
 return {
   font = wezterm.font({
     family = "Maple Mono",
-    harfbuzz_features = { "calt", "ss01" },
+    harfbuzz_features = { "dlig" },
   }),
+  font_size = 14,
   color_scheme = "Dracula",
   hide_tab_bar_if_only_one_tab = true,
 }
