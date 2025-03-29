9b1d27ac60ef59f1169b4d75dea0f8e67f06e700
Binh Tran <binhtran432k@gmail.com>
2025-03-29 10:42:55 +0700

    feat: add pnpm

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,3 +2,10 @@ if status is-interactive
     fish_config theme choose Dracula
     fish_vi_key_bindings
 end
+
+# pnpm
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
